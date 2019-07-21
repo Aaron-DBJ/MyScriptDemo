0991d3190aa7cb1770019404eb6a680784792ea9
Aaron <1195923123@qq.com>
2019-07-21 18:25:51 +0800

    添加bitmap工具类

--- app/src/main/java/com/example/myscriptdemo/utils/BitmapUtil.java
+++ app/src/main/java/com/example/myscriptdemo/utils/BitmapUtil.java
@@ -1,26 +1,32 @@
 package com.example.myscriptdemo.utils;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import java.io.File;
+import java.io.InputStream;
 
 public class BitmapUtil {
     private static BitmapUtil instance;
-    private static final int FROM_BYTEARRAY = 0;
-    private static final int FROM_FILE = 1;
-    private static final int FROM_FILE_DESCRIPTOR = 2;
-    private static final int FROM_SRTEAM = 3;
-    private static final int FROM_RESOURCE = 4;
+    private Context context;
+//    private static final int FROM_BYTEARRAY = 0;
+//    private static final int FROM_FILE = 1;
+//    private static final int FROM_FILE_DESCRIPTOR = 2;
+//    private static final int FROM_SRTEAM = 3;
+//    private static final int FROM_RESOURCE = 4;
 
-    private BitmapUtil(){
-
+    private BitmapUtil(Context context){
+        this.context = context;
     }
 
-    public static BitmapUtil getInstance(){
+    public static BitmapUtil getInstance(Context context){
         if (instance == null){
             synchronized (BitmapUtil.class){
                 if (instance == null){
-                    instance = new BitmapUtil();
+                    instance = new BitmapUtil(context);
                 }
             }
         }
@@ -39,14 +45,37 @@ public class BitmapUtil {
         return inSampleSize;
     }
 
-    private Bitmap decodeInSampledBitmap(int from){
-        switch (from){
-            case FROM_BYTEARRAY:
-
-        }
+    private Bitmap decodeInSampledBitmapFromResource(int resId,  int reqWidth, int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(context.getResources(), resId, options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(context.getResources(), resId);
     }
-    private void decodeInSampledBitmap(BitmapFactory.Options options, int reqWidth, int reqHeight){
-        BitmapFactory.
+
+    private Bitmap decodeInSampledBitmapFromStream(InputStream inputStream, int reqWidth, int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, new Rect(0,0,0,0), options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeStream(inputStream,new Rect(0,0,0,0), options);
+    }
+
+    private Bitmap decodeInSampledBitmapFromFile(String file, int reqWidth, int reqHeight){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(file, options);
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(file, options);
+    }
+
+    private Bitmap compressPixelByte(String file, Bitmap.Config config){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = config;
+        return BitmapFactory.decodeFile(file, options);
     }
 
 
